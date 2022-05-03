d883a3f76361d265dee34865b42a77bdf8435068
Fran Iglesias <fran.iglesias@codurance.com>
2022-05-03 17:34:29 +0200

    Initial commit, empty project with first scenario definition

---
+++ src/test/java/flowapowa/StepDefinitions.java
@@ -0,0 +1,31 @@
+package flowapowa;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepDefinitions {
+    @Given("{string} costs {double} {string}")
+    public void costs(String string, Double double1, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("crafting costs {int}%")
+    public void crafting_costs(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I request a bouquet with {int} {string}")
+    public void i_request_a_bouquet_with(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the total price is {double} {string}")
+    public void the_total_price_is(Double double1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
